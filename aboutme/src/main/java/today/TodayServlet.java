47c25263d0f67be402d06b8569262153386fa261
tokkissi <alal4674@gmail.com>
2022-07-19 20:26:53 +0900

    aboutme.html 글 내용 수정, TodayServlet 작성

--- aboutme/src/main/java/today/TodayServlet.java
+++ aboutme/src/main/java/today/TodayServlet.java
@@ -1,6 +1,10 @@
 package today;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -26,6 +30,23 @@ public class TodayServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ddd
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("현재시간 : yyyy/M/d H:m");
+		String nowString = now.format(dateTimeFormatter);
+		
+		out.println("<!DOCTYPE html><head><style>body {height: 100%} a {font-size: 2rem;} p {font-size: 4rem; margin: 12rem 0; text-align: center;}</style></head><body>");
+		out.println("<a href=\"http://localhost:8080/aboutme/index.html\">메인화면</a>");
+		out.println("<p>" + nowString + "</p>");
+		out.println("</body></html>");
+		
+//		out.println("<a href=\"http://localhost:8080/aboutme/index.html\" style=\"font-size:2rem\">메인화면</a>");
+//		out.println("<h1>" + nowString + "</h1>");
+	}
+		
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+		doGet(request, response);
 	}
 }
